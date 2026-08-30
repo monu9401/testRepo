0723dd053d581cc2373cc13900e1ef637a2762b6
Monu Nigam <monu.nigam.bit@gmail.com>
2026-08-30 10:46:02 +0530

    Implement ProductController with product listing

---
+++ TestApp/src/main/java/com/TestApp/Main/Controller/MyController.java
@@ -0,0 +1,37 @@
+package com.TestApp.Main.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+  
+    private final List<Product> products = new ArrayList<>;
+    Product p1 = new Product(101,"Phone",50000);
+    Product p2 = new Product(102,"Laptop",100000);
+    Product p3 = new Product(103,"Data Cable",500);
+    products.add(p1);
+    products.add(p2);
+    products.add(p3);
+  
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(products);
+    }
+}
+class Product{
+  int id;
+  String name;
+  int price;
+
+  public Product(int id,String name,int price){
+    this.id=id;
+    this.name=name;
+    this.price=price;
+  }
+}
